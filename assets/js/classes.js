8b0d040947ae0412655eb5aa9acea0efd333dcb1
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-10-04 09:57:16 +0300

    Finish 2nd part

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -30,34 +30,76 @@ const user1 = new User1(
 
 class User{
     constructor(name, surname, age, isMale, email, isSubscribed) {
-        this.firstName = name;
-        this.lastName = surname;
-        this.age = age;
-        this.isMale = isMale;
-        this.email = email;
-        this.isSubscribed = isSubscribed;
+        this._firstName = name;
+        this._lastName = surname;
+        this._age = age;
+        this._isMale = isMale;
+        this._email = email;
+        this._isSubscribed = isSubscribed;
+    };
+
+// user2.age = 20    
+    set age(value) {
+        if(typeof value !== 'number') {
+            throw new TypeError('age must be number value')
+        };
+
+        if(value < 0 || value > 150) {
+            throw new RangeError ('age must be between 0 and 150');
+        };
+
+        this._age = value;
+    };
+
+// const age = user2.age
+    get age() {
+        return this._age;
     };
 
     getFullName() {
-        return `${this.firstName} ${this.lastName}`;
+        return `${this._firstName} ${this._lastName}`;
     };
 
+// статичний метод - працює для класа а не для його екземпляра
     static isUser(obj) {
         return obj instanceof User;
     };
 };
 
-const user2 = new User(
-    'jack',
-    'wilsher',
-    23,
-    true,
-    'test@mail.com',
-    true
-);
+try {
+    const user2 = new User(
+        'jack',
+        'wilsher',
+        23,
+        true,
+        'test@mail.com',
+        true
+    );
 
-console.log(user2.getFullName());
+    console.log(user2.getFullName());
+    console.log(user2.age);
 
+    user2.age = 23;
+} catch(error) {
+    console.log(error);
+};
 
+// реалізувати клас для телефонів
 
+class Phones {
+    constructor (brand, model, color, price, year) {
+        this._brand = brand;
+        this._model = model;
+        this._color = color;
+        this._price = price;
+        this._year = year;
+    };
 
+    calcPhoneAge() {
+        return new Date().getFullYear() - this._year;
+    };
+};
+
+const iPhone = new Phones('Apple', 'iphone', 'white', 100, 2023);
+
+console.log(iPhone.calcPhoneAge());
